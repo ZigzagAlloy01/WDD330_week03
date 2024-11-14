286e0c4d5b44078ec3193c6877f7ebbf70795366
TheeJStar <kingoflightj@gmail.com>
2024-11-14 08:05:10 -0800

    fixed the cart item replacement bug

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,22 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // Checking if the element is already in the card
+  const alreadyInCart = cart.find((element) => element.Id === product.Id);
+
+  if (alreadyInCart) {
+    alreadyInCart.Quantity += 1;
+  } else {
+    product.Quantity = 1;
+    cart.push(product);
+  }
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
