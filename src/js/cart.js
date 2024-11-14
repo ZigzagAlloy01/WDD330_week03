9e3cf14466cd1dd6e5c65507c843b39e90ceb01d
anxbell <valerieanabell@gmail.com>
2024-11-14 00:46:21 -0600

    cart img

--- src/js/cart.js
+++ src/js/cart.js
@@ -16,7 +16,7 @@ function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
+      src="/images/tents/${item.Image}"
       alt="${item.Name}"
     />
   </a>
